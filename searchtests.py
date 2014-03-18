e0fa2c0e13f2b7dd0a31b50e55a7f646eed3e4c5
Vladimir <sanofi@bk.ru>
2014-03-18 17:01:25 -0500

    Added Binary Search tests

--- searchtests.py
+++ searchtests.py
@@ -3,6 +3,7 @@
 
 import unittest
 import linearsearch
+import binarysearch 
 
 class SearchTests(unittest.TestCase):
 
@@ -11,7 +12,12 @@ class SearchTests(unittest.TestCase):
         self.B = []
         self.C = [2, -1]
         self.D = [1, 1, 2, 1, 0]
+        self.E = [1, 2, 3, 8, 9, 10, 12, 15, 20]
+        self.F = [4]
 
+    '''
+        Linear Search tests
+    '''
     def test_ls_index(self):
         index = linearsearch.linearsearch(self.A, 2)
         self.assertEqual(3, index)
@@ -24,6 +30,20 @@ class SearchTests(unittest.TestCase):
         index = linearsearch.linearsearch(self.B, 2)
         self.assertEqual(-1, index)
     
+    '''
+        Binary Search tests
+    '''
+    def test_bs_True(self):
+        self.assertTrue(binarysearch.binarysearch(self.E, 12))
+
+    def test_bs_False(self):
+        self.assertFalse(binarysearch.binarysearch(self.E, -1))
+
+    def test_bs_Empty(self):
+        self.assertFalse(binarysearch.binarysearch(self.B, 12))
+
+    def test_bs_Single(self):
+        self.assertTrue(binarysearch.binarysearch(self.F, 4))
 
 if '__main__' == __name__:
     unittest.main()
