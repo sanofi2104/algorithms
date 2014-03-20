27e25c9d29d1d7b29aa5a4387563f1b0085fb099
Vladimir <sanofi@bk.ru>
2014-03-20 11:37:26 -0500

    Created Binary Tree search algorithm

---
+++ binarytree.py
@@ -0,0 +1,72 @@
+import Queue 
+
+class binary_node(object):
+
+    def __init__(self, _value):
+        self.value = _value
+        self.left = None
+        self.right = None
+       
+
+'''
+Breadth-First search
+'''
+def breadth_first(_tree):
+    if not _tree:
+        return False
+
+    result = []
+    queue = Queue.Queue()
+    queue.put(_tree)
+
+    while not queue.empty():
+        temp = queue.get()
+        result.append(temp.value)
+
+        if temp.left:
+            queue.put(temp.left)
+        if temp.right:
+            queue.put(temp.right)
+        
+    return result 
+            
+'''
+Depth-first search 
+'''
+# Pre-Order depth-first searching algorithm
+def pre_order(_node, result = []):
+    if _node == None:
+        return
+    
+    result.append(_node.value)
+    pre_order(_node.left, result)
+    pre_order(_node.right, result)
+    
+    return result
+
+
+# In-Order depth-first searching algorithm
+def in_order(_node, result = []):
+    if _node == None:
+        return
+    
+    in_order(_node.left, result)
+    result.append(_node.value)
+    in_order(_node.right, result)
+    
+    return result
+
+
+# Post-Order depth-first searching algorithm
+def post_order(_node, result = []):
+    if _node == None:
+        return
+    
+    post_order(_node.left, result)
+    post_order(_node.right, result)
+    result.append(_node.value)
+    
+    return result
+
+
+
